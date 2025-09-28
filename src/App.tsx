83fd0c71656a78d87c9c7dff1d0592ee4dd48fc1
[jeahunDeskTop] <[jeahun1@naver.com]>
2025-09-29 00:56:21 +0900

    1111

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import ExcelInsertPage from './page/ExelInsertPage'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ExcelInsertPage from "./pages/ExelInsertPage";
+import NewCompaniesPage from "./pages/NewCompaniesPage";
 
-
-function App() {
-
+export default function App() {
   return (
-    <>
-<ExcelInsertPage></ExcelInsertPage>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Navigate to="/excel" replace />} />
+      <Route path="/excel" element={<ExcelInsertPage />} />
+      <Route path="/excel/new" element={<NewCompaniesPage />} />
+    </Routes>
+  );
 }
-
-export default App
