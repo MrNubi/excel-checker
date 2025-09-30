a1d8ee3730f3ee1c90cbcbbeb79cd0c838b50c9d
jaehun choi <99630193+MrNubi@users.noreply.github.com>
2025-09-30 11:17:21 +0900

    Update App.tsx
    실내용없음

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ExcelInsertPage from "./pages/ExelInsertPage";
 import NewCompaniesPage from "./pages/NewCompaniesPage";
-
+// strict 모드 체크
 export default function App() {
   return (
     <Routes>
